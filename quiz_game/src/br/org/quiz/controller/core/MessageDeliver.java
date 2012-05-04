93479fb4f1f30dbe44d62c33d03363e1130575b6
Matheus Binotto <matheus@localhost.localdomain>
2012-05-04 17:13:42 -0300

    Acerto nos conflitos

---
+++ quiz_game/src/br/org/quiz/controller/core/MessageDeliver.java
@@ -0,0 +1,28 @@
+package br.org.quiz.controller.core;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class MessageDeliver {
+
+	
+	public static void addSuccessMessage() {
+		
+		FacesMessage message = new FacesMessage();
+		message.setDetail("Operação realizada com sucesso.");
+		message.setSummary("OK! ");
+		message.setSeverity( FacesMessage.SEVERITY_INFO );
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+	
+	public static void addErrorMessage(String error) {
+		
+		FacesMessage message = new FacesMessage();
+		message.setDetail(error);
+		message.setSummary("Erro. ");
+		message.setSeverity( FacesMessage.SEVERITY_ERROR );
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+	
+	
+}
