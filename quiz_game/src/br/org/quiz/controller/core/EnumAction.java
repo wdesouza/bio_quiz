c67492f36965852ecb60725c2abd41aa5685caa5
Welliton de Souza <well309@gmail.com>
2012-05-11 18:07:53 -0300

    Adicionado cabeçalho de licença; arquivo da licença

--- quiz_game/src/br/org/quiz/controller/core/EnumAction.java
+++ quiz_game/src/br/org/quiz/controller/core/EnumAction.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2012 Matheus Binotto, Welliton de Souza
+ *
+ * Este arquivo é parte do programa BioQuiz
+ *
+ * BioQuiz é um software livre; você pode redistribui-lo e/ou 
+ * modifica-lo dentro dos termos da Licença Pública Geral GNU como 
+ * publicada pela Fundação do Software Livre (FSF); na versão 2 da 
+ * Licença, ou (na sua opnião) qualquer versão.
+ *
+ * Este programa é distribuido na esperança que possa ser  util, 
+ * mas SEM NENHUMA GARANTIA; sem uma garantia implicita de ADEQUAÇÂO a qualquer
+ * MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+ * Licença Pública Geral GNU para maiores detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU
+ * junto com este programa, se não, escreva para a Fundação do Software
+ * Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package br.org.quiz.controller.core;
 
 public enum EnumAction {
